4f36a0ac22436f4b80fcdf69bbcc745ea07856d3
siddarth kotthur <kotthursiddarth@gmail.com>
2023-06-27 19:06:01 -0500

    demo api working 🤓

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from flask import Flask, request
+from urllib.parse import urlparse
 
-captions = YouTubeTranscriptApi.get_transcript("XjV4HYZTJB8")
 # convert SRT to text
 
-final_text = ""
+app = Flask('API')
 
-for time_stamp in captions:
-    final_text += time_stamp["text"]
-    final_text += " "
+
+@app.route('/summarize', methods=['GET'])
+def hello():
+    args = request.args
+    youtube_link = args.get('link')
+
+    url_parse = urlparse(youtube_link)
+    youtube_id = url_parse.query.split("=")[1]
+
+    captions = srt = YouTubeTranscriptApi.get_transcript(youtube_id)
+
+    final_text = ""
+
+    for time_stamp in captions:
+        final_text += time_stamp["text"]
+        final_text += " "
+
+    return {"Text": final_text, "Summary": "In Progress"}
